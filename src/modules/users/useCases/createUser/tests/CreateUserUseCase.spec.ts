e998eaa6fb2b24417605ecaa62bdf9005c1b0f48
Tundê <ayotunde_sales@hotmail.com>
2022-03-29 14:08:33 -0300

    feat(users): add create user tests

---
+++ src/modules/users/useCases/createUser/tests/CreateUserUseCase.spec.ts
@@ -0,0 +1,43 @@
+import { AppError } from "../../../../../shared/errors/AppError"
+import { InMemoryUsersRepository } from "../../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "../CreateUserUseCase"
+
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let createUserUseCase: CreateUserUseCase
+
+describe("test", () => {
+
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+  })
+
+  it("Should be able to create a new user", async() => {
+    const user = await createUserUseCase.execute({
+      name: "Fake User",
+      email: "fake@email.com",
+      password: "123456"
+    })
+
+    expect(user).toHaveProperty("id")
+    expect(user.email).toEqual("fake@email.com")
+  })
+
+  it("Should not be able to create a new user with a email already registered", async() => {
+    expect(async() => {
+      await createUserUseCase.execute({
+        name: "Fake User",
+        email: "fake@email.com",
+        password: "123456"
+      })
+
+      await createUserUseCase.execute({
+        name: "Fake User",
+        email: "fake@email.com",
+        password: "123456"
+      })
+
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
